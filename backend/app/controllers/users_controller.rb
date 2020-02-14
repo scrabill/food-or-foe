3660d301ecf3d4b45323c20f2fc6c05f4c97d0a0
scrabill <shannon.crabill@gmail.com>
2020-02-13 23:03:03 -0500

    Adds users controller

---
+++ backend/app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class API::VI::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+
+    render json: @users, status: 200
+  end
+
+  def show
+    @user = User.find_by(name: params[:name])
+
+    render json: @user, status: 200
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    render json: @user, status: 200
+  end
+
+  def update
+    @user = User.find_by(name: params[:name])
+    @user.update(user_params)
+
+    render json: @user, status: 200
+  end
+
+  private
+
+    def user_params
+      params.require(:game).permit(:user, :score)
+    end
+end
