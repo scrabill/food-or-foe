a66e6ea1ba37564cad8e11d96470efc83ab8aee2
scrabill <shannon.crabill@gmail.com>
2020-02-13 22:35:52 -0500

    Allow CORS

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -33,5 +33,14 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Allow CORS for local testing
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+          origins '*'
+          resource '*', headers: :any, methods: [:get, :post]
+      end
+    end
+
   end
 end
