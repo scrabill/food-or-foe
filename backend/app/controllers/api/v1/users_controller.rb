7a22c3e5b95e7f787dccedb6d022f98c5c95862f
scrabill <shannon.crabill@gmail.com>
2020-02-16 17:09:57 -0500

    Show nested games for users

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users, status: 200
+    render json: @users.to_json(include: [:game]), status: 200
   end
 
   def show
