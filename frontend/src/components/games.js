846ad0036fd34aa4b53e7b4d91a9519de146c5f0
scrabill <shannon.crabill@gmail.com>
2020-02-18 18:07:12 -0500

    Moves game functionality to Game class and file

--- frontend/src/components/games.js
+++ frontend/src/components/games.js
@@ -6,9 +6,76 @@ class Games {
     this.fetchAndLoadGames()
   }
 
+  // Inherit properys and bindings, etc to emojis and games? Extends?
   initBindingsAndEventListeners() {
     this.leaderboard = document.querySelector("#leaderboard")
+
+    this.counterContainer = document.querySelector("#counter")
+
+    this.startGame = document.querySelector("#start")
+    this.startGame.addEventListener('click', (e) => {
+      console.log("THE GAME HAS BEGUN")
+      // Enable game buttons
+      foodButton.removeAttribute("disabled")
+      foeButton.removeAttribute("disabled")
+
+      // Start the timer
+      this.startTimer()
+      // emojis = new Emojis()
+
+      console.log(emojis.length)
+
+    });
   }
+
+  // TODO: Update counter in UI
+
+  startTimer() {
+    console.log("⏰")
+
+    // let counter = 3
+    //
+    // while (counter > 0) {
+    //   window.setInterval(this.printTime, 1000);
+    //   counter = - 1
+    // }
+
+    // let timer = window.setInterval(this.printTime, 1000);
+    console.log(this.counterContainer)
+
+    let counter = this.counterContainer.textContent;
+
+    let printTime = setInterval(function() {
+        counter--;
+        console.log(counter)
+        // console.log(this)
+        // this.counterContainer.innerText = counter;
+        if (counter <= 0) clearInterval(printTime);
+    }, 1000);
+
+
+
+  }
+
+  // printTime() {
+  //   console.log("hii")
+  //   console.log(counter)
+  //   // console.log(this.currentCount)
+  //
+  //   // if (this.currentCount != 0) {
+  //   //   this.currentCount --;
+  //   //   document.getElementById("counter").innerHTML = this.currentCount;
+  //   // } else if (this.currentCount == 0 ) {
+  //   //   console.log(this.currentCount)
+  //   //   clearInterval(timer)
+  //   //   console.log(this.currentCount)
+  //   //
+  //   //   // displayScore()
+  //   //   // clearInterval(timer)
+  //   //   console.log("Times up!")
+  //   // }
+  //
+  // }
 
   fetchAndLoadGames() {
     console.log("Bzz. Loading Games...")
