fdf79cd202cd9066658f22eeb6fe9bcbbc424f06
scrabill <shannon.crabill@gmail.com>
2020-02-19 22:09:45 -0500

    Adds URL for emoji food scope

--- frontend/src/adapters/EmojisAdapter.js
+++ frontend/src/adapters/EmojisAdapter.js
@@ -1,9 +1,14 @@
 class EmojisAdapter {
   constructor() {
     this.baseURL = "http://localhost:3000/api/v1/emojis"
+    this.baseURLFoodScope = "http://localhost:3000/api/v1/emojis/food"
   }
 
   getEmojis() {
     return fetch(this.baseURL).then(res => res.json())
+  }
+
+  getFoodEmojis() {
+    return fetch(this.baseURLFoodScope).then(res => res.json())
   }
 }
