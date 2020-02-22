6c60a1e4846c2778a95262442c69738e79f4c478
scrabill <shannon.crabill@gmail.com>
2020-02-21 20:00:32 -0500

    Creates a new user when via a form

--- frontend/src/adapters/UsersAdapter.js
+++ frontend/src/adapters/UsersAdapter.js
@@ -6,4 +6,15 @@ class UsersAdapter {
   getUsers() {
     return fetch(this.baseURL).then(res => res.json())
   }
+
+  createUser(data) {
+    return fetch(this.baseURL, {
+      method: 'POST',
+      headers: {
+         'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+  }
 }
--- frontend/src/components/users.js
+++ frontend/src/components/users.js
@@ -2,7 +2,18 @@ class Users {
   constructor() {
     this.users = []
     this.adapter = new UsersAdapter()
+    this.initBindingsAndEventListeners()
     this.fetchAndLoadUsers()
+  }
+
+  initBindingsAndEventListeners() {
+    this.name = document.querySelector("#name")
+
+    this.submitButton = document.querySelector("#submit")
+    this.submitButton.addEventListener('click', (e) => {
+      this.createUser(e)
+    });
+
   }
 
   fetchAndLoadUsers() {
@@ -13,6 +24,17 @@ class Users {
     .then(() => {
       this.render()
     })
+  }
+
+  // TODO: Find or create by name
+  createUser(e) {
+    e.preventDefault
+
+    const data = {
+      name: this.name.value
+    }
+
+    this.adapter.createUser(data)
   }
 
   render() {
