b792c1214f36d15151355d03dae115462aecd031
scrabill <shannon.crabill@gmail.com>
2020-02-23 16:57:47 -0500

    Updates seed data with more emojis, user and scores to prepopulate the database

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,13 +6,44 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Emoji.create(slug: "burger", character: "🍔")
-Emoji.create(slug: "rocket ship", character: "🚀")
-Emoji.create(slug: "unicorn", character: "🦄")
-Emoji.create(slug: "shrimp", character: "🍤")
-Emoji.create(slug: "grapes", character: "🍇")
-Emoji.create(slug: "watermelon", character: "🍉")
-Emoji.create(slug: "lemon", character: "🍋")
-Emoji.create(slug: "shrimp", character: "🍤")
-Emoji.create(slug: "robot", character: "🤖")
-Emoji.create(slug: "light bulb", character: "💡")
+# Creates a user
+User.create(name: "Shigetaka Kurita")
+
+# Creates food emojis...yum!
+Emoji.create(slug: "burger", character: "🍔", is_food?: true)
+Emoji.create(slug: "shrimp", character: "🍤", is_food?: true)
+Emoji.create(slug: "grapes", character: "🍇", is_food?: true)
+Emoji.create(slug: "watermelon", character: "🍉", is_food?: true)
+Emoji.create(slug: "lemon", character: "🍋", is_food?: true)
+Emoji.create(slug: "avocado", character: "🥑", is_food?: true)
+Emoji.create(slug: "pretzel", character: "🥨", is_food?: true)
+Emoji.create(slug: "cheese", character: "🧀", is_food?: true)
+Emoji.create(slug: "taco", character: "🌮", is_food?: true)
+Emoji.create(slug: "spaghetti", character: "🍝", is_food?: true)
+Emoji.create(slug: "coconut", character: "🥥", is_food?: true)
+Emoji.create(slug: "carrot", character: "🥕", is_food?: true)
+Emoji.create(slug: "croissant", character: "🥐", is_food?: true)
+Emoji.create(slug: "pancakes", character: "🥞", is_food?: true)
+Emoji.create(slug: "banana", character: "🍌", is_food?: true)
+
+# Creates foe emojis...please do not eat them
+Emoji.create(slug: "rocket ship", character: "🚀", is_food?: false)
+Emoji.create(slug: "unicorn", character: "🦄", is_food?: false)
+Emoji.create(slug: "shrimp", character: "🍤", is_food?: false)
+Emoji.create(slug: "robot", character: "🤖", is_food?: false)
+Emoji.create(slug: "light bulb", character: "💡", is_food?: false)
+Emoji.create(slug: "floppy disk", character: "💾", is_food?: false)
+Emoji.create(slug: "books", character: "📚", is_food?: false)
+Emoji.create(slug: "moneybag", character: "💰", is_food?: false)
+Emoji.create(slug: "bomb", character: "💣", is_food?: false)
+Emoji.create(slug: "bicycle", character: "🚲", is_food?: false)
+Emoji.create(slug: "anchor", character: "⚓", is_food?: false)
+Emoji.create(slug: "alarm clock", character: "⏰", is_food?: false)
+Emoji.create(slug: "magnifying glass", character: "🔍", is_food?: false)
+Emoji.create(slug: "abacus", character: "🧮", is_food?: false)
+Emoji.create(slug: "gem stone", character: "💎", is_food?: false)
+
+# Creates some games and scores to populate the leaderboard
+5.times do
+  order = Game.create!(user_id: 1, score: Faker::Number.within(range: 19..45)])
+end
