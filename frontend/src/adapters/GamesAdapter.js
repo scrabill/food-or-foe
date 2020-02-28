fdb22e2ee6ea24f35649f474433fd4e91f6ba54e
scrabill <shannon.crabill@gmail.com>
2020-02-28 16:24:45 -0500

    Update get Games to fetch sorted games list, instead of the sequental version

--- frontend/src/adapters/GamesAdapter.js
+++ frontend/src/adapters/GamesAdapter.js
@@ -1,10 +1,10 @@
 class GamesAdapter {
   constructor() {
-    this.baseURL = "http://localhost:3000/api/v1/games/leaderboard"
+    this.baseURL = "http://localhost:3000/api/v1/games"
   }
 
   getGames() {
-    return fetch(this.baseURL).then(res => res.json())
+    return fetch(`${this.baseURL}/leaderboard`).then(res => res.json())
   }
 
   createGame(data) {
